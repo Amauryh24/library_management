0f74d4ffed8c81b74bba76588572320a2eabdaef
Amauryh24 <amaury.henrottay@gmail.com>
2024-06-10 16:11:10 +0200

    add basic model and view for category and rename file properly

--- models/library_book.py
+++ models/library_book.py
@@ -1,7 +1,7 @@
 from odoo import fields, models
 
 
-class Book(models.Model):
+class LibraryBook(models.Model):
     _name = 'library.book'
     _description = 'Library Book'
 
---
+++ models/library_book_category.py
@@ -0,0 +1,8 @@
+from odoo import fields, models
+
+
+class LibraryBookCategory(models.Model):
+    _name = 'library.book.category'
+    _description = 'Library Book Category'
+
+    name = fields.Char()
