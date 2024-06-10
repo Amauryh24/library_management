a8471bf55a55de59522a9c3ee804a369ea2ea496
Amauryh24 <amaury.henrottay@gmail.com>
2024-06-10 13:38:44 +0200

    Add book model

---
+++ models/book.py
@@ -0,0 +1,17 @@
+from odoo import fields, models
+
+
+class Book(models.Model):
+    _name = 'library.book'
+    _description = 'Library Book'
+
+    name = fields.Char(string='Title', required=True)
+    author = fields.Char(string='Author')
+    isbn = fields.Integer()
+    # Borrowed  -> when another user has the book
+    # Travaling -> when the book is inside a book box
+    status = fields.Selection([
+        ('available', 'Available'),
+        ('borrowed', 'Borrowed'),
+        ('traveling', 'Traveling')
+    ], string='Status', defaul='available')
