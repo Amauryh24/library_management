6712a5e5946a0a3c850709239feadb552015e045
Amauryh24 <amaury.henrottay@gmail.com>
2024-06-13 17:14:20 +0200

    Add domain on field book_ids

--- models/res_user.py
+++ models/res_user.py
@@ -4,4 +4,4 @@ from odoo import fields, models
 class Users(models.Model):
     _inherit = 'res.users'
 
-    book_ids = fields.One2many("library.book", "owner_id")
+    book_ids = fields.One2many("library.book", "owner_id", domain=lambda self: [('state', '=', 'available')])
